5538d4c7e1aaad27d7afff2ad48c93250943e85a
pm-akshay <akshaymohanan2007@gmail.com>
2026-08-15 20:04:59 +0530

    Add maxSubarraySum function using Kadane's algorithm
    Implement Kadane's algorithm to find maximum subarray sum.

---
+++ kadane.c
@@ -0,0 +1,15 @@
+int maxSubarraySum(int arr[], int n) {
+    // Code here
+    int i;
+    int curr_sum=0,max_sum=0;
+    for(i=0;i<n;i++){
+        curr_sum+=arr[i];
+        if (curr_sum > max_sum) {
+            max_sum = curr_sum;
+        }
+        if(curr_sum<0){
+            curr_sum=0;
+        }
+    }
+    return max_sum;
+}
